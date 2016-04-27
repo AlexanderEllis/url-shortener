17d57b6f7492d0494b4ce980f357d25e7fe695f1
Alexander Ellis <ellisonjtk@gmail.com>
2016-04-27 11:10:47 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,36 @@
+var http = require('http');
+var express = require('express');
+var app = express();
+var urlList = {};
+var url;
+
+
+app.get('/*', function(req, res) {
+	if (req.url.indexOf("http://www.") < 0) {
+		url = req.url;
+		url = url.slice(1,url.length);
+		if (urlList[url] === undefined) {
+			res.end("Incorrect URL");
+			return;
+		}
+		else {
+		res.redirect(urlList[url]);
+		return;
+		}
+	}
+	var result = {};
+	var potato = Math.floor(Math.random() * 9999);
+	url = req.url;
+	url = url.slice(1,url.length);
+	urlList[potato] = url;
+	result.originalUrl = url;
+	result.shortUrl = "localhost:5000/" + potato;
+	result = JSON.stringify(result);
+	res.end(result);
+})
+
+var port = process.env.PORT || 3000;
+
+app.listen(port, function(){
+	console.log("Server listening at:" + port);
+});
